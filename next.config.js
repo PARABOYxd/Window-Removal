dc6a6806c03d06804c1f7787d15187d493195f22
Rishab <jainrishab9821@gmail.com>
2025-10-05 16:53:04 +0530

    CHANGE

--- next.config.js
+++ next.config.js
@@ -4,7 +4,16 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
